a5509c7ed6caebb58016dd7d4037bfcb4f910b2f
arjunp8822 <106944140+arjunp8822@users.noreply.github.com>
2024-01-20 21:22:19 +1100

    deploy update

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -9,17 +9,7 @@ interface Props {
 
 const Slider = ({ initial, min, max, step }: Props) => {
   const [value, setValue] = useState(initial.toPrecision(1));
-  return (
-    <div>
-      <ReactSlider
-        className="horizontal-slider"
-        thumbClassName="example-thumb"
-        trackClassName="example-track"
-        renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
-      />
-      {value}
-    </div>
-  );
+  return <div>{value}</div>;
 };
 
 export default Slider;

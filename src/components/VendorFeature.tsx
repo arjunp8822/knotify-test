8aa053edb087b74031703a0e2fe3f79fc23ec4e2
arjunp8822 <106944140+arjunp8822@users.noreply.github.com>
2024-01-26 19:39:27 +1100

    added listing page

--- src/components/VendorFeature.tsx
+++ src/components/VendorFeature.tsx
@@ -4,25 +4,33 @@ interface Props {
     content: string;
     img: string;
     comingSoon: boolean;
-    reverse: boolean;
+    benefit: string;
+    benefitIcon: string;
   };
 }
 
 const VendorFeature = ({ data }: Props) => {
   return (
-    <div className="flex flex-col lg:flex-row gap-4 lg:gap-8 justify-center items-center">
-      <img src={data.img} alt={data.title} className="w-full lg:w-1/2" />
-      <div className="w-full lg:w-1/2 flex flex-col gap-2">
+    <div className="flex flex-col gap-4 lg:gap-8 justify-center items-center shadow-[rgba(0,_0,_0,_0.25)_0px_25px_50px_-12px] overflow-hidden p-2 rounded">
+      <div className="relative">
+        <img
+          src={data.img}
+          alt={data.title}
+          className="object-cover w-[450px]"
+        />
         {data.comingSoon && (
-          <span className="bg-green-500 text-white rounded-xl p-1 w-fit">
+          <span className="bg-green-500 text-white rounded-xl px-2 py-1 w-fit absolute top-2 right-2 text-sm">
             Coming Soon
           </span>
         )}
-        <h5 className="text-2xl lg:text-3xl font-semibold mb-4">
-          {data.title}
-        </h5>
-
-        <p className="text-md lg:text-lg">{data.content}</p>
+      </div>
+      <div className="flex flex-col gap-2 h-[500px] px-4">
+        <h5 className="text-xl font-semibold mb-4">{data.title}</h5>
+        <p className="text-md">{data.content}</p>
+        <div className="mt-8 flex items-center gap-4 border px-2 py-1">
+          <img src={data.benefitIcon} alt={"Icon"} className="w-6" />
+          <span>{data.benefit}</span>
+        </div>
       </div>
     </div>
   );
--- src/components/VendorFeatures.tsx
+++ src/components/VendorFeatures.tsx
@@ -1,54 +1,66 @@
 import VendorFeature from "./VendorFeature";
-import SampleImg from "/assets/venues/black.png";
+import Listing from "/assets/Listing.png";
+import Edit from "/assets/Edit.png";
+import Bookings from "/assets/Bookings.png";
+import LiveChat from "/assets/LiveChat.png";
+import CRM from "/assets/CRM.png";
+import BNPL from "/assets/BNPL.png";
+import cash from "/assets/benefits/cash.png";
 
 const features = [
   {
     comingSoon: false,
-    reverse: false,
     title: "Your Personalised Listing",
     content:
       "With Knotify listings, your services are strategically showcased to thousands of couples actively seeking your expertise for their special day. Elevate your visibility within your local demographic through our comprehensive directory listing and tap into targeted searches with our geo-specific app features.",
-    img: SampleImg,
+    benefit: "This is a test benefit",
+    benefitIcon: cash,
+    img: Listing,
   },
   {
     comingSoon: true,
-    reverse: true,
     title: "Effortless Vendor Profile Management",
     content:
       "Revolutionise the way you manage your vendor presence with our user-friendly listing updates. Our vendor onboarding process has been redesigned to eliminate the need for manual entries, offering vendors a seamless and efficient solution for keeping their profiles up-to-date. Embrace simplicity as you effortlessly modify and enhance your vendor page, ensuring that you stay in sync with the dynamic needs of your business.",
-    img: SampleImg,
+    benefit: "",
+    benefitIcon: "",
+    img: Edit,
   },
   {
     comingSoon: true,
-    reverse: false,
     title: "Integrated Booking Experience",
     content:
       "Embark on a new era of efficiency with our expanded application, now equipped with an integrated booking system. Experience the convenience of managing all your bookings seamlessly within our platform, eliminating the need for third-party solutions.",
-    img: SampleImg,
+    benefit: "",
+    benefitIcon: "",
+    img: Bookings,
   },
   {
     comingSoon: true,
-    reverse: true,
     title: "Live Chat With Couples",
     content:
       "Say goodbye to extensive phone calls and embrace a cost-effective, time-saving alternative. Studies show that over 70% of Gen-Z and millennials prefer digital channels for communication, making our live chat a pivotal tool for reaching your target audience effectively.",
-    img: SampleImg,
+    benefit: "",
+    benefitIcon: "",
+    img: LiveChat,
   },
   {
     comingSoon: true,
-    reverse: false,
     title: "Integrated CRM and Seamless File Management",
     content:
       "Empowering vendors to streamline their operations, our platform enables end-to-end customer interaction management within a singular, comprehensive solution. Say goodbye to scattered systems and welcome the simplicity of having your customer relationship management and file storage seamlessly integrated. From tracking interactions to securely managing files, our platform offers a centralized hub for enhancing productivity and organisation.",
-    img: SampleImg,
+    benefit: "",
+    benefitIcon: "",
+    img: CRM,
   },
   {
     comingSoon: true,
-    reverse: true,
     title: "Book Now Pay Later",
     content:
       "This groundbreaking feature not only facilitates improved cash flow but also transforms the payment process. Vendors can now receive payments in full upon contract signing, while couples gain the flexibility to pay in convenient instalments. Say goodbye to delayed payments and embrace a seamless financial experience that benefits both vendors and couples alike.",
-    img: SampleImg,
+    benefit: "",
+    benefitIcon: "",
+    img: BNPL,
   },
 ];
 
@@ -56,7 +68,7 @@ const VendorFeatures = () => {
   return (
     <div className="mt-8 mb-16 sm:mb-24">
       <h3 className="mb-12 text-2xl lg:text-3xl font-semibold">Our features</h3>
-      <div className="flex flex-col gap-12 justify-center items-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {features.map((f) => (
           <VendorFeature data={f} />
         ))}

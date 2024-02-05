8e3d906245d14a9b9317e7ed7c886802cfc9ea03
arjunp8822 <106944140+arjunp8822@users.noreply.github.com>
2024-02-05 16:45:36 +1100

    added vendor type descriptions

--- src/components/venues/VendorType.tsx
+++ src/components/venues/VendorType.tsx
@@ -19,13 +19,41 @@ const vendorTypeList: vendorTypeInterface[] = [
     description:
       "From pricing and reviews to capacity and available packages. Delve into the options, shortlist your favourites, and explore regions that resonate with your vision.",
   },
-  { title: "Florists", description: "" },
-  { title: "Photographers", description: "" },
-  { title: "Cakes", description: "" },
-  { title: "Entertainment", description: "" },
-  { title: "MCs", description: "" },
-  { title: "Catering", description: "" },
-  { title: "Transportation", description: "" },
+  {
+    title: "Florists",
+    description:
+      "Discover a floral paradise with our extensive listing of florists. Unearth the perfect bloom for your special day by exploring pricing, customer reviews, and unique arrangements.",
+  },
+  {
+    title: "Photographers",
+    description:
+      "Capture the essence of your love story with our curated list of talented photographers. Explore their portfolios, pricing, and customer reviews to find the perfect match for your style.",
+  },
+  {
+    title: "Cakes",
+    description:
+      "Indulge your sweet tooth and make a statement with a delectable wedding cake. Explore our diverse range of bakers offering various flavours, designs, and sizes.",
+  },
+  {
+    title: "Entertainment",
+    description:
+      "Elevate your wedding festivities with our entertainment options. From live bands and DJs to dancers and performers, discover the perfect act to keep your guests entertained.",
+  },
+  {
+    title: "MCs",
+    description:
+      "Explore our list of charismatic and experienced MCs, complete with pricing and reviews. Choose an MC who can add a touch of personality and professionalism to your event.",
+  },
+  {
+    title: "Catering",
+    description:
+      "Delight your guests' taste buds with our diverse catering options. Explore menus, pricing, and customer reviews to find a caterer that can tailor their offerings to your preferences.",
+  },
+  {
+    title: "Transportation",
+    description:
+      "Arrive in style and leave a lasting impression with our transportation options. Explore a range of vehicles, from classic cars to luxury limousines.",
+  },
 ];
 
 const VendorType = ({ filtersSelected, setFiltersSelected }: Props) => {

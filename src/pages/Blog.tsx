3eb921b4964b8d8a27384361ccdd8541b67776a9
arjunp8822 <106944140+arjunp8822@users.noreply.github.com>
2024-02-27 11:07:13 +1100

    fixed blog page

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,22 +1,72 @@
+import { Link } from "react-router-dom";
+import blog1 from "/assets/blogs/blog0101.jpg";
+import blog2 from "/assets/blogs/blog0201.jpg";
+import blog3 from "/assets/blogs/blog0301.jpg";
+
 const Blog = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {/* Copy for component */}
-      <div className="bg-red-50 h-[450px] p-2 sm:p-4">
-        <div className="bg-green-50 h-[55%]">Image</div>
-        <div className="h-[30%]">
-          <h2 className="mt-4">Title</h2>
-          <p className="mt-2">Description</p>
-        </div>
-        <button className="border">Read More</button>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
+      <div className="h-[500px] sm:h-[600px] flex flex-col gap-4">
+        <img
+          src={blog1}
+          alt="Wedding Blog"
+          className="w-full object-cover h-[300px]"
+        />
+        <h2 className="sm:text-lg font-semibold pt-2 h-[60px] flex items-center">
+          Navigating the Wedding Planning Maze
+        </h2>
+        <p className="text-sm sm:text-base text-gray-500 h-[150px] font-light overflow-hidden text-ellipsis">
+          Embarking on the journey of planning your wedding can be both
+          thrilling and daunting, especially for those who are new to the
+          process. With countless decisions to make and endless details to
+          consider, navigating the wedding planning maze can feel overwhelming.
+        </p>
+        <button className="bg-primary text-white text-sm sm:text-base uppercase">
+          <Link to="/navigating-the-wedding-planning-maze">Read More</Link>
+        </button>
       </div>
-      <div className="bg-red-50 h-[450px]">Blog</div>
-      <div className="bg-red-50 h-[450px]">Blog</div>
-      <div className="bg-red-50 h-[450px]">Blog</div>
-      <div className="bg-red-50 h-[450px]">Blog</div>
-      <div className="bg-red-50 h-[450px]">Blog</div>
-      <div className="bg-red-50 h-[450px]">Blog</div>
-      <div className="bg-red-50 h-[450px]">Blog</div>
+      <div className="h-[500px] sm:h-[600px] flex flex-col gap-4">
+        <div className="h-[500px] sm:h-[600px] flex flex-col gap-4">
+          <img
+            src={blog2}
+            alt="Wedding Blog"
+            className="w-full object-cover h-[300px]"
+          />
+          <h2 className="sm:text-lg font-semibold pt-2 h-[60px] flex items-center">
+            10 On-the-Day Wedding Tips for Couples
+          </h2>
+          <p className="text-sm sm:text-base text-gray-500 h-[150px] font-light overflow-hidden text-ellipsis">
+            Your wedding day is one of the most significant moments in your
+            life, filled with love, joy, and the promise of a lifetime together.
+            However, amidst the excitement, it's easy for couples to feel
+            overwhelmed by the myriad of details and responsibilities.
+          </p>
+          <button className="bg-primary text-white text-sm sm:text-base uppercase">
+            <Link to="/ten-on-the-day-wedding-tips">Read More</Link>
+          </button>
+        </div>
+      </div>
+      <div className="h-[500px] sm:h-[600px] flex flex-col gap-4">
+        <div className="h-[500px] sm:h-[600px] flex flex-col gap-4">
+          <img
+            src={blog3}
+            alt="Wedding Blog"
+            className="w-full object-cover h-[300px]"
+          />
+          <h2 className="sm:text-lg font-semibold pt-2 h-[60px] flex items-center">
+            5 Tips for a Stress-Free Celebration
+          </h2>
+          <p className="text-sm sm:text-base text-gray-500 h-[150px] font-light overflow-hidden text-ellipsis">
+            Celebrations mark significant moments in our lives, offering
+            opportunities to create lasting memories with loved ones. However,
+            the pressure of planning and executing a flawless event can often
+            lead to unnecessary stress.
+          </p>
+          <button className="bg-primary text-white text-sm sm:text-base uppercase">
+            <Link to="/tips-for-a-stress-free-celebration">Read More</Link>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
